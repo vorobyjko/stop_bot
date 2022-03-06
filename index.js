c2f6ec4053cdc438cdfe2287736e5f687be4a9c1
kostiantyn.bogapov <kostiantyn.bogapov@playtika.com>
2022-03-06 19:24:46 +0200

    save

--- index.js
+++ index.js
@@ -8,13 +8,21 @@ if (process.env.API_KEY === undefined) {
 const socialNetworks = {
   facebook: 'Facebook',
   instagram: 'Instagram',
-  vk: 'VK'
+  vk: 'VK',
+  YouTube: 'YouTube',
+  TikTok: 'TikTok',
+  Telegram: 'Telegram'
+}
+
+const resetScene = async (ctx) => {
+  await ctx.scene.leave();
+  return ctx.scene.enter('super-wizard');
 }
 
 const firstKeyboard = async (ctx) => {
   return ctx.replyWithHTML(
     '<b>Please choose social network</b>',
-    Markup.keyboard([...Object.values(socialNetworks)])
+    Markup.keyboard(Object.values(socialNetworks))
       .oneTime()
       .resize()
   )
@@ -27,21 +35,33 @@ firstStep.command('start', async (ctx) => {
 })
 
 const stepHandler = new Composer();
-stepHandler.hears(/Facebook|Instagram|VK/, async (ctx) => {
-  await ctx.replyWithHTML(
-    `<b>You choose: ${ctx.message.text}</b>`,
-    Markup.keyboard([['Confirm', '⬅️ Change']])
+stepHandler.hears(/Facebook|Instagram|VK|YouTube|TikTok|Telegram/, async (ctx) => {
+  ctx.wizard.state.socialNetwork = ctx.message.text;
+  return await ctx.replyWithHTML(
+    `You choose: <b>${ctx.message.text}</b>. Confirm?`,
+    Markup.keyboard([['Confirm']])
       .oneTime()
       .resize()
   )
-  ctx.wizard.state.socialNetwork = ctx.message.text;
-  return ctx.wizard.next();
-  // return ctx.wizard.steps[ctx.wizard.cursor](ctx)
-})
 
-stepHandler.use((ctx) =>
-  ctx.replyWithMarkdown('Please choose social network')
-)
+  // return ctx.wizard.steps[ctx.wizard.cursor].handler(ctx)
+})
+stepHandler.hears(/Confirm/, async (ctx) => {
+  await ctx.wizard.next();
+  return ctx.wizard.steps[ctx.wizard.cursor].handler(ctx);
+})
+stepHandler.command('start', async (ctx) => {
+  return resetScene(ctx);
+})
+stepHandler.use((ctx) => {
+  const msg = ctx.wizard.state.socialNetwork ? 'Please confirm social network' : 'Please choose social network';
+  return ctx.replyWithMarkdown(msg);
+})
+/*handleBack.hears(/⬅️ Change/, async (ctx) => {
+  ctx.wizard.back();
+  ctx.wizard.back();
+  return ctx.wizard.steps[ctx.wizard.cursor].handler(ctx);
+})*/
 
 const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 const provideLinkStep = new Composer();
@@ -53,10 +73,12 @@ provideLinkStep.hears(/⬅️ Change/, async (ctx) => {
   return await firstKeyboard(ctx);
 })
 provideLinkStep.hears(expression, async (ctx) => {
-  await ctx.reply('Link was added');
   ctx.wizard.state.link = ctx.message.text;
   ctx.wizard.next();
-  return ctx.wizard.steps[ctx.wizard.cursor](ctx);
+  return ctx.wizard.steps[ctx.wizard.cursor].handler(ctx);
+})
+provideLinkStep.command('start', async (ctx) => {
+  return resetScene(ctx);
 })
 provideLinkStep.use((ctx) =>
   ctx.replyWithMarkdown('Please enter valid link')
@@ -64,33 +86,113 @@ provideLinkStep.use((ctx) =>
 
 const confirmStep = new Composer();
 confirmStep.hears(/Confirm/, async (ctx) => {
-  let processing = true;
+  if (!ctx.wizard.state.processing) {
+    ctx.wizard.state.processing = true;
 
-  const doc = new GoogleSpreadsheet('1W2SY5fXBixxwv_S3pUQ5aHEJ-7L7dzpKKiSVTQ88svc');
-  await doc.useServiceAccountAuth({
-    client_email: process.env.CLIENT_EMAIL,
-    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-  });
+    const doc = new GoogleSpreadsheet('1W2SY5fXBixxwv_S3pUQ5aHEJ-7L7dzpKKiSVTQ88svc');
+    await doc.useServiceAccountAuth({
+      client_email: process.env.CLIENT_EMAIL,
+      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    });
 
-  await ctx.reply('processing...')
-  await doc.loadInfo();
-  const sheet = doc.sheetsByIndex[0];
-  const rows = await sheet.getRows();
-  const canAdd = rows.length === 0 || !rows.some(row => row.link.includes(ctx.message.text));
+    await ctx.reply('processing...')
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    const rows = await sheet.getRows();
+    const canAdd = rows.length === 0 || !rows.some(row => row['Link'] === ctx.wizard.state.link);
 
-  if (canAdd) {
-    await sheet.addRow({link: ctx.wizard.state.link})
-    await ctx.reply('Done');
-    return await ctx.scene.leave();
-  } else {
-    await ctx.replyWithHTML('Link already exists!');
-    return await ctx.scene.leave();
+    if (canAdd) {
+      await sheet.addRow({
+        ['Network']: ctx.wizard.state.socialNetwork,
+        ['Link']: ctx.wizard.state.link,
+        ['Followers']: ctx.wizard.state.followers,
+        ['Details']: ctx.wizard.state.details
+      })
+      await ctx.reply('✅ Done');
+      ctx.wizard.state.processing = false;
+      await ctx.scene.leave();
+      return ctx.reply('Please click on /start if you want to add more links');
+    } else {
+      await ctx.reply('❌ Link already exists!');
+      await ctx.scene.leave();
+      return ctx.reply('Please click on /start if you want to add another link');
+      /*await ctx.reply('Please enter another link:');
+      confirmStep.hears(expression, async () => {
+        ctx.wizard.state.link = ctx.message.text;
+        ctx.wizard.next();
+        return ctx.wizard.steps[ctx.wizard.cursor].handler(ctx);
+      })*/
+      // await ctx.wizard.back();
+      // await ctx.wizard.back();
+      // await ctx.wizard.back();
+      // return ctx.wizard.steps[ctx.wizard.cursor].handler(ctx);
+    }
   }
 })
-confirmStep.hears(/⬅️ Back/, async (ctx) => {
+/*confirmStep.hears(/⬅️ Back/, async (ctx) => {
   ctx.wizard.back();
   ctx.wizard.back();
   return ctx.wizard.steps[ctx.wizard.cursor](ctx);
+})*/
+
+confirmStep.command('start', async (ctx) => {
+  return resetScene(ctx)
+})
+
+const numberOfFollowers = {
+  0: '0-1000',
+  1: '1000-10000',
+  2: '10000-100000',
+  3: '> 100000',
+  /*4: '⬅️ Back',*/
+}
+
+const followersStep = new Composer();
+followersStep.hears(/0-1000|1000-10000|10000-100000|> 100000|[0-9]+/, async (ctx) => {
+  await ctx.wizard.next();
+  return ctx.wizard.steps[ctx.wizard.cursor].handler(ctx);
+})
+followersStep.command('start', async (ctx) => {
+  return resetScene(ctx)
+})
+
+followersStep.use(async (ctx) => {
+  return await ctx.replyWithHTML(
+    `<b>Please enter or select number of followers:</b>`,
+    Markup.keyboard(Object.values(numberOfFollowers))
+      .oneTime()
+      .resize()
+  )
+})
+const detailsStep = new Composer();
+detailsStep.on('text', async (ctx) => {
+  await ctx.reply('Provide additional details:')
+  ctx.wizard.state.followers = ctx.message.text;
+  ctx.wizard.next();
+})
+
+const finalizeStep = new Composer();
+finalizeStep.hears(/Confirm/, async (ctx) => {
+  ctx.wizard.next();
+  return ctx.wizard.steps[ctx.wizard.cursor].handler(ctx);
+})
+finalizeStep.command('start', async (ctx) => {
+  return resetScene(ctx)
+})
+finalizeStep.use(async (ctx) => {
+  ctx.wizard.state.details = ctx.message.text;
+  const {socialNetwork, followers, link, details = ''} = ctx.wizard.state;
+
+  return await ctx.replyWithHTML(
+    `<b>Please confirm provided information:</b>
+Social Network: <b>${socialNetwork}</b>
+Followers: <b>${followers}</b>
+Details: <b>${details}</b>
+Link: <b>${link}</b>`,
+    Markup.keyboard([['Confirm']])
+      .oneTime()
+      .resize()
+  )
 })
 
 const superWizard = new Scenes.WizardScene(
@@ -98,34 +200,11 @@ const superWizard = new Scenes.WizardScene(
   firstStep,
   stepHandler,
   provideLinkStep,
-  async (ctx) => {
-    await ctx.replyWithHTML(
-      `<b>Please enter or select number of followers:</b>`,
-      Markup.keyboard([['0-1000'], ['1000-10000'], ['10000-100000'], ['> 100000'], ['⬅️ Back']])
-        .oneTime()
-        .resize()
-    )
-
-    return ctx.wizard.next();
-  },
-  async (ctx) => {
-    ctx.wizard.state.followers = ctx.message.text;
-    const {socialNetwork, followers, link} = ctx.wizard.state;
-
-    await ctx.replyWithHTML(
-      `<b>Please confirm provided information:</b>
-Social Network: <b>${socialNetwork}</b>
-Followers: <b>${followers}</b>
-Link: <b>${link}</b>`,
-      Markup.keyboard([['Confirm', '⬅️ Back']])
-        .oneTime()
-        .resize()
-    )
-    return ctx.wizard.next();
-  },
+  followersStep,
+  detailsStep,
+  finalizeStep,
   confirmStep
 )
-
 
 const bot = new Telegraf(process.env.API_KEY)
 const stage = new Scenes.Stage([superWizard], {
@@ -138,5 +217,5 @@ bot.use(stage.middleware())
 bot.launch()
 
 // Enable graceful stop
-// process.once('SIGINT', () => bot.stop('SIGINT'))
-// process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
